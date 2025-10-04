920e74ef6820ebc3904766880c57806298cb820c
R6S <ravishraj@gmail.com>
2025-10-04 23:28:21 +0530

    del connnff

--- .config/script/rofi.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rofi -show drun -run-command 'bash -c "paplay /usr/share/sounds/freedesktop/stereo/dialog-information.oga; exec \"$@\"" _ {cmd}'
--- .config/script/ss.h
+++
@@ -1,3 +0,0 @@
-image=$(cat .cache/wal/wal)
-walogram -i "$image"
-
