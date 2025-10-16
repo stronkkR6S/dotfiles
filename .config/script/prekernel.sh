a4af110f4ab0b2ee0ff58335da2f1c8fd457a79d
R6S <ravishraj@gmail.com>
2025-10-16 19:45:45 +0530

    changes the path

--- .config/script/prekernel.sh
+++ .config/script/prekernel.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+# run inside /usr/src
 set -e
 echo "Setting active kernel..."
 eselect kernel set 1
 echo "Cleaning previous build..."
+cd linux
 make mrproper
-if [[ ! -f ~/config/gentoo/final ]]; then
+if [[ ! -f /home/ravish/config/gentoo/final ]]; then
     echo "Old config file not found!"
     exit 1
 fi
 echo "copying your old .config to your new config"
-cp ~/config/gentoo/final .config
+cp /home/ravish/config/gentoo/final .config
 echo "Updating kernel config..."
 make oldconfig
 make -j$(nproc) && make modules_install -j$(nproc) && make install
+echo " updating uefi config"
+uefi-mkconfig
 read -p "Do you want to reboot now? [y/N]: " answer
 if [[ "$answer" =~ ^[Yy]$ ]]; then
     echo "Rebooting..."
